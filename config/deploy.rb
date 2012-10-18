b0ee2369d0271edca4a29d1d2aa20c8f89aae1ae
Rob Nichols <git@nicholshayes.co.uk>
2012-10-18 08:55:14 +0100

    Adds production asset handling to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,6 +14,8 @@ set :user, 'rnic'
 set :use_sudo, false
 set :scm_username, 'rnic'
 
+load 'deploy/assets'
+
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
 
