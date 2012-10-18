ea98fce1a044937ed00883d90de204e16aa4b623
Rob Nichols <git@nicholshayes.co.uk>
2012-10-18 16:27:50 +0100

    fixed production config issues

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :repository, "file:///var/www/git/self_assessment.git"
 set :local_repository,  "ssh://rnic@wvtcent1/var/www/git/self_assessment.git"
 
 set :scm, :git
-set :deploy_to, '~/public_html'
+set :deploy_to, '~/public_html/self_assessment'
 
 # run 'bundle cache' and commit the gems put into vendor/cache to git master before running deploy
 set :bundle_flags, '--local'
@@ -23,3 +23,11 @@ load 'deploy/assets'
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
 
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
