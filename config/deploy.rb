7d4251d12c766d3f8353a370389715259df9578d
Rob Nichols <git@nicholshayes.co.uk>
2012-10-18 12:53:28 +0100

    Deploy now works with cached gems and bundler

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,5 @@
+require "bundler/capistrano"
+
 set :application, "Self assessment"
 set :repository, "file:///var/www/git/self_assessment.git"
 set :local_repository,  "ssh://rnic@wvtcent1/var/www/git/self_assessment.git"
@@ -5,10 +7,12 @@ set :local_repository,  "ssh://rnic@wvtcent1/var/www/git/self_assessment.git"
 set :scm, :git
 set :deploy_to, '~/public_html'
 
+# run 'bundle cache' and commit the gems put into vendor/cache to git master before running deploy
+set :bundle_flags, '--local'
+
 role :web, "wvtcent1"                          # Your HTTP server, Apache/etc
 role :app, "wvtcent1"                          # This may be the same as your `Web` server
 role :db,  "wvtcent1", :primary => true        # This is where Rails migrations will run
-#role :db,  "your slave db-server here"
 
 set :user, 'rnic'
 set :use_sudo, false
@@ -19,14 +23,3 @@ load 'deploy/assets'
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
