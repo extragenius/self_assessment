798bcc1116059e59fbeec47fb819a6e337f653a1
Rob Nichols <git@nicholshayes.co.uk>
2012-10-19 10:51:05 +0100

    Tidies up deploy by arranging statements in related groups

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,6 @@
 require "bundler/capistrano"
 
 set :application, "Self assessment"
-set :repository, "file:///var/www/git/self_assessment.git"
-set :local_repository,  "ssh://rnic@wvtcent1/var/www/git/self_assessment.git"
-
-set :scm, :git
-set :deploy_to, '~/public_html/self_assessment'
-
-# run 'bundle cache' and commit the gems put into vendor/cache to git master before running deploy
-set :bundle_flags, '--local'
 
 role :web, "wvtcent1"                          # Your HTTP server, Apache/etc
 role :app, "wvtcent1"                          # This may be the same as your `Web` server
@@ -16,7 +8,15 @@ role :db,  "wvtcent1", :primary => true        # This is where Rails migrations 
 
 set :user, 'rnic'
 set :use_sudo, false
+set :deploy_to, '~/public_html/self_assessment'
+
+set :scm, :git
 set :scm_username, 'rnic'
+set :repository, "file:///var/www/git/self_assessment.git"
+set :local_repository,  "ssh://rnic@wvtcent1/var/www/git/self_assessment.git"
+
+# run 'bundle cache' and commit the gems put into vendor/cache to git master before running deploy
+set :bundle_flags, '--local'
 
 load 'deploy/assets'
 
@@ -24,10 +24,10 @@ load 'deploy/assets'
 # after "deploy:restart", "deploy:cleanup"
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
