3c908ec22246f6f7accd5dc60c054517d05f9e58
Rob Nichols <git@nicholshayes.co.uk>
2012-11-30 15:08:43 +0000

    Adds side bar with rules set syntax examples to admin rule_sets views

--- app/admin/rule_sets.rb
+++ app/admin/rule_sets.rb
@@ -11,18 +11,50 @@ ActiveAdmin.register RuleSet do
 
   form :partial => 'admin/rule_sets/form'
 
-#  form do |f|
-#    f.inputs "Details" do
-#      f.input :title
-#      f.input :description, :as => :ckeditor, :input_html => { :height => 100, :toolbar => 'Basic' }
-#      f.input :url, :as => :url
-#      f.input :answers, :as => :check_boxes, :collection => Answer.order(:question_id).collect{|a| ["#{a.value}:- #{a.question.title}", a.id]}
-#    end
-#    f.buttons
-#  end
-
   show do
     render :template => 'admin/rule_sets/show'
+  end
+  
+  sidebar :rule_syntax do
+    para 'R1 = "(a1 and a2) or (a3 and a4)"'
+
+    para 'R2 = "a1 and not a2"'
+
+    para 'R3 = "2 in a1 a2 a3"'
+
+    para 'R4 = "(2 in a1 a2 a3) and (1 in a4 a5)"'
+    
+    table(:class => 'small') do
+      tr do
+        th "Input"
+        th "R1"
+        th "R2"
+        th "R3"
+        th "R4"
+      end
+      tr do
+        th "[a1, a2]"
+        th "true"
+        th "false"
+        th "true"
+        th "false"
+      end
+      tr do
+        th "[a3, a4]"
+        th "true"
+        th "false"
+        th "false"
+        th "false"
+      end
+      tr do
+        th "[a1, a3, a5]"
+        th "false"
+        th "true"
+        th "true"
+        th "true"
+      end      
+    end
+
   end
   
 end
