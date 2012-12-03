e70d9147634cb4574cf03a5b3ad12da5f66d3e96
Rob Nichols <git@nicholshayes.co.uk>
2012-12-03 15:45:46 +0000

    Adds clean up after deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :bundle_flags, '--local'
 load 'deploy/assets'
 
 # if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+after "deploy:restart", "deploy:cleanup"
 
 # If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
