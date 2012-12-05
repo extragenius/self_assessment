6e801956632971f0d50b2fe557481718d1a44ceb
Rob Nichols <git@nicholshayes.co.uk>
2012-12-05 14:52:12 +0000

    Updated seeds so that questionnaires etc. are not added if there are already multiple questionnaires in the system

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,19 +8,24 @@ Seeder.monitor Answer
 Seeder.monitor RuleSet
 Seeder.process_log.start('first questionnaire questions', 'Questionnaire.count > 0 ? Questionnaire.first.questions.length : 0')
 
-Seeder.objects_from('questions.yml').each do |holder, data|
-  questionnaire = Questionnaire.find_or_create_by_title(data['title'])
-  
-  if data['questions']
-    data['questions'].values.each do |question_data| 
-      question = Question.find_or_create_by_title(question_data['question'])
-      question.create_standard_answers
-      answer = question.answers.find_or_create_by_value('Yes')
-      rule_set = RuleSet.find_or_initialize_by_title(question_data['title'])
-      rule_set.url = question_data['url']
-      rule_set.answers = [answer]
-      rule_set.save
-      questionnaire.questions << question
+if Questionnaire.count > 2
+  puts "**** Bypassing questionnaire, question, answer and rule set population"
+  puts "**** as there are already #{Question.count} questionnaires in the system"
+else
+  Seeder.objects_from('questions.yml').each do |holder, data|
+    questionnaire = Questionnaire.find_or_create_by_title(data['title'])
+
+    if data['questions']
+      data['questions'].values.each do |question_data|
+        question = Question.find_or_create_by_title(question_data['question'])
+        question.create_standard_answers
+        answer = question.answers.find_or_create_by_value('Yes')
+        rule_set = RuleSet.find_or_initialize_by_title(question_data['title'])
+        rule_set.url = question_data['url']
+        rule_set.answers = [answer]
+        rule_set.save
+        questionnaire.questions << question
+      end
     end
   end
 end
