a13d45057b4389f6cb6096f79ea1baefbf77083a
Rob Nichols <git@nicholshayes.co.uk>
2012-12-11 11:49:04 +0000

    Allows warnings to be edited within admin

--- app/admin/ominous_warnings.rb
+++ app/admin/ominous_warnings.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Ominous::Warning do
   
+  actions :all, :except => [:destroy]
+  
   index do
     column :name
     column :closers do |warning|
@@ -9,6 +11,61 @@ ActiveAdmin.register Ominous::Warning do
         end
       end
     end
+    default_actions
+  end
+  
+  show do
+    h3 'Closers'
+    
+    table :class => 'sortable_list' do
+      tr do
+        th 'Name'
+        th 'Closure method'
+        th 'Starts hidden'        
+        th 'Text'
+        th 'Link text'
+      end
+      ominous_warning.closers.each do |closer|
+        tr do
+          td closer.name
+          td closer.closure_method.humanize
+          td (closer.start_hidden? ? 'true' : 'false')        
+          td t("ominous.warning.#{ominous_warning.name}.#{closer.name}.message")
+          td t("ominous.warning.#{ominous_warning.name}.#{closer.name}.link")
+        end
+      end
+    end
+    
+    para 'Text and link text defined in locale file (e.g. /config/locales/en.yml)'
+    
+    h3 'Closure method behaviours'
+    table do
+      Ominous::Closer.closure_methods.each do |name, behaviour|
+        tr do
+          td name.to_s.humanize
+          td behaviour
+        end
+      end
+    end
+  end
+  
+  form do |f|
+    f.inputs "Warning" do
+      f.input :name
+    end
+    
+    f.has_many :closers do |closer_form|
+      closer_form.input :name, :label => 'Name used in locale file'
+      closer_form.input :url, :label => 'Url: Only for redirects'
+      closer_form.input(
+        :closure_method, 
+        :as => :select, 
+        :collection => Ominous::Closer.closure_methods.keys
+      )
+      closer_form.input :start_hidden
+    end
+    
+    f.buttons
   end
   
 end
