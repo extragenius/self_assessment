dc5f9559f6b65bf6c000ffa0f2e78398b7b210a4
Rob Nichols <git@nicholshayes.co.uk>
2012-12-14 09:57:58 +0000

    Turns off asset compression in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,17 +9,17 @@ SelfAssessment::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = false
   config.assets.debug = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
-  config.assets.digest = true
+  config.assets.digest = false
 
   # Precompile ckeditor js and css files   ,
   config.assets.precompile += [Ckeditor.assets, 'ckeditor/init.js'].flatten
