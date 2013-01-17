edb371bdc6b11d6995a834a4c1e064fef0bab821
Rob Nichols <git@nicholshayes.co.uk>
2013-01-17 09:51:33 +0000

    Refactors overriding of Qwester::RuleSet, to use decorator pattern

--- app/models/qwester/rule_set.rb
+++
@@ -1,7 +0,0 @@
-require "#{Qwester::Engine.config.root}/app/models/qwester/rule_set"
-
-module Qwester
-  class RuleSet < ActiveRecord::Base
-    belongs_to :warning, :class_name => 'Ominous::Warning'
-  end
-end
