7569af450ba884aa9b7e405b057a662991c4362a
Rob Nichols <git@nicholshayes.co.uk>
2013-01-22 15:39:53 +0000

    Temporarily disable qwester initializer until tables migrated

--- config/initializers/qwester.rb
+++ config/initializers/qwester.rb
@@ -1,12 +1,12 @@
-require "#{Rails.root}/app/decorators/models/qwester/rule_set_decorator"
-
-# map existing constants to Qwester versions
-
-Answer = Qwester::Answer
-AnswerStore = Qwester::AnswerStore
-Question = Qwester::Question
-Questionnaire = Qwester::Questionnaire
-RuleSet = Qwester::RuleSet
-
-
-Qwester.active_admin_menu = 'Questionnaire items'
+#require "#{Rails.root}/app/decorators/models/qwester/rule_set_decorator"
+#
+## map existing constants to Qwester versions
+#
+#Answer = Qwester::Answer
+#AnswerStore = Qwester::AnswerStore
+#Question = Qwester::Question
+#Questionnaire = Qwester::Questionnaire
+#RuleSet = Qwester::RuleSet
+#
+#
+#Qwester.active_admin_menu = 'Questionnaire items'
