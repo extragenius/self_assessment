84ef902e8ed76d798eaa6d26998986c9168ce479
Rob Nichols <git@nicholshayes.co.uk>
2013-03-27 15:16:50 +0000

    Sets target as an allowable attribute for sanitized html tags

--- config/application.rb
+++ config/application.rb
@@ -59,5 +59,7 @@ module SelfAssessment
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.action_view.sanitized_allowed_attributes = ['target']
   end
 end
