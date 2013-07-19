f5245e4e47eb26f1c783076ce9ad89e55ce7f394
Rob Nichols <git@nicholshayes.co.uk>
2013-07-19 09:16:47 +0100

    Disable remote partials until JQuery conflict issue fixed

--- config/initializers/remote_partial.rb
+++ config/initializers/remote_partial.rb
@@ -1,19 +1,21 @@
-protocol = Rails.env == 'production' ? 'https' : 'http'
+# TODO Re-enable when styles no longer cause JQuery conflict
 
-RemotePartial.define(
-  url: "#{protocol}://www.warwickshire.gov.uk/tpheadersa1",
-  name: 'tpheadersa1',
-  minimum_life: 6.hours
-)
-
-RemotePartial.define(
-  url: "#{protocol}://www.warwickshire.gov.uk/tpfootersa",
-  name: 'tpfootersa',
-  minimum_life: 6.hours
-)
-
-RemotePartial.define(
-  url: "#{protocol}://www.warwickshire.gov.uk/tpbarsa",
-  name: 'tpbarsa',
-  minimum_life: 6.hours
-)
+#protocol = Rails.env == 'production' ? 'https' : 'http'
+#
+#RemotePartial.define(
+#  url: "#{protocol}://www.warwickshire.gov.uk/tpheadersa1",
+#  name: 'tpheadersa1',
+#  minimum_life: 6.hours
+#)
+#
+#RemotePartial.define(
+#  url: "#{protocol}://www.warwickshire.gov.uk/tpfootersa",
+#  name: 'tpfootersa',
+#  minimum_life: 6.hours
+#)
+#
+#RemotePartial.define(
+#  url: "#{protocol}://www.warwickshire.gov.uk/tpbarsa",
+#  name: 'tpbarsa',
+#  minimum_life: 6.hours
+#)
