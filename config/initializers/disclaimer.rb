c5a724eb3e21ce5f85f39fc869bd06bfac45daf4
Rob Nichols <git@nicholshayes.co.uk>
2013-09-16 10:01:43 +0100

    Disables disclaimer root hack

--- config/initializers/disclaimer.rb
+++ config/initializers/disclaimer.rb
@@ -1,4 +1,4 @@
 # Assumes when in production, app will be hosted at /<app_folder>/
-if Rails.env == 'production'
-  Disclaimer.host_app_root_path= Rails.root.to_s.match(/\/\w+$/)[0]
-end
+#if Rails.env == 'production'
+#  Disclaimer.host_app_root_path= Rails.root.to_s.match(/\/\w+$/)[0]
+#end
