990e324005a0333a260d166cd7770722311706b4
Rob Nichols <git@nicholshayes.co.uk>
2013-09-16 17:31:15 +0100

    Modifies presentation show/hide to store state in cookie

--- app/assets/javascripts/presentation.js
+++ app/assets/javascripts/presentation.js
@@ -2,23 +2,40 @@ $(function(){
   var showText = 'Show information';
   var hideText = 'Hide information';
   var buttonClass = 'desc_hide_show';
-  var startHeight = '0';
-  var endHeight = $('.presentation .description').height();
-  $('.presentation .description').css({height:startHeight, overflow:'hidden'});
-  $('.presentation h2').before( '<button class="'+ buttonClass + ' btn btn-info">' + showText + '</botton>');
+  var lastAction = 'lastPresentationDesc';
+  var nextAction = 'nextPresentationDesc';
+
+  $('.presentation h2').before( '<button class="'+ buttonClass + ' btn btn-info">' + showText + '</button>');
+  
+  function showPresentationDesc(action) {
+    var presentation = $('.presentation');
+    var description = presentation.find('.description');
+    var button = presentation.find("." + buttonClass);
+    var speed = 400;
+
+    if (action === 'hide') {
+      description.hide(speed);
+      button.html(showText);
+      $.cookie(nextAction, 'show', {path: '/'});
+      $.cookie(lastAction, 'hide', {path: '/'});
+    }
+    else {
+      description.show(speed);
+      button.html(hideText);
+      $.cookie(nextAction, 'hide', {path: '/'});
+      $.cookie(lastAction, 'show', {path: '/'});
+    }
+
+    console.log('This action = ' + action);
+    console.log('Last action = ' + $.cookie(lastAction));
+    console.log('Next action = ' + $.cookie(nextAction));
+  }
+  
   $('.presentation .' + buttonClass).click(function() {
-      $this = $(this);
-      if ($this.data('open')) {
-          $this.parent().find('.description').animate({height:startHeight});
-          $this.data('open', 0);
-          $this.html(showText);
-      }
-      else {
-          $this.parent().find('.description').animate({height:endHeight});
-          $this.data('open', 1);
-          $this.html(hideText);
-      }
+    showPresentationDesc($.cookie(nextAction));
   });
+
+  showPresentationDesc($.cookie(lastAction));
 });
 
 
